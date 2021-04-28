62ef8982e66352fd76b26bd4698eab7058434cf6
Ajay <ajay.meena68@yahoo.in>
2021-04-28 20:10:33 +0530

    Commit from CLI

---
+++ Class1.vb
@@ -0,0 +1,12 @@
+﻿Public Class TestPlugin
+    Dim Test As String
+    Public Function CountIndicator(ByVal Count As Integer, ByRef Message As String)
+        If Count = 100 Then
+            Message = "Percent"
+        ElseIf Count > 120 Then
+            Message = "Slightly Higher"
+        Else
+            Message = "Above"
+        End If
+    End Function
+End Class
---
+++ Sympraxis.Plugins.TestPlugin_UnitTest/UnitTest1.vb
@@ -0,0 +1,28 @@
+﻿Imports System.Text
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+Imports Sympraxis.Plugins.TestPlugin
+<TestClass()> Public Class UnitTest1
+
+
+    Dim MyPlugin As New TestPlugin.TestPlugin
+
+    <TestMethod()> _
+    Public Sub CountIndicator_Senario1_Above200()
+        Dim sMessage As String
+        MyPlugin.CountIndicator(200, sMessage)
+        Assert.AreEqual("Slightly Higher", sMessage)
+    End Sub
+    <TestMethod()> _
+    Public Sub CountIndicator_Senario2_Above200()
+        Dim sMessage As String
+        MyPlugin.CountIndicator(200, sMessage)
+        Assert.AreNotEqual("Above", sMessage)
+    End Sub
+    <TestMethod()> _
+    Public Sub CountIndicator_Senario1_100()
+        Dim sMessage As String
+        MyPlugin.CountIndicator(100, sMessage)
+        Assert.AreEqual("Percent", sMessage)
+    End Sub
+
+End Class
